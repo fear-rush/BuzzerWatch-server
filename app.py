e7bcb94ab912df7099ef60b0f827072a8017c9cf
muhammadfiras <muhfiras1@gmail.com>
2022-05-30 23:05:06 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+
+from sentiment import sentiment
+app = Flask(__name__)
+CORS(app)
+
+# @app.route("/")
+# def home():
+#     return render_template('index.html')
+
+@app.route("/twitter", methods=['POST'])
+def result():
+    data = request.json['hashtag']
+    print(data)
+    # text = request.values['hashtag']
+    # print(text)
+    res = sentiment(data)
+    responses = res.to_json(orient = "table")
+    return responses
+
+if __name__ == '__main__':
+    app.run(debug=True)
